02f34fce78b2b625bdf3035c0682b626c37c807e
QuentinPapp <Quentin.papp@outlook.fr>
2017-01-16 09:38:20 +0100

    V1.1

--- nodexwood/pageCatalogue.php
+++ nodexwood/pageCatalogue.php
@@ -1,22 +1,33 @@
-<?php require_once 'classes/classeClients.php';
+<?php require 'classes/classeClients.php';
 session_start();
-require_once 'classes/classeArticle.php';
-require_once 'classes/mappeur.php'; ?>
+require 'classes/classeArticle.php';
+require 'classes/mappeur.php'; ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Preselection</title>
+	<link rel="icon" type="image/png" href="favicon.png" />
+	<title>NODEX | Catalogue</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 </head>
-<body>
-	<main>
+<body class="body_catalogue">
+	<section>
 		<article>
-				<h1><?php echo "Bonjour ".$_SESSION["client"]->getPrenom()." ".$_SESSION["client"]->getNom(); ?></h1>
+				<center><h1 class="titre_bonjour"><?php echo "Bonjour ". "<span>" .$_SESSION["client"]->getPrenom()." ".$_SESSION["client"]->getNom();?></span></h1></center>
 				<?php $catalogue = Mapper::getCatalogue($_SESSION["client"]->getTypologie());
 				 for ($i=0; $i < count($catalogue); $i++)
 					include 'objectCatalogue.php';
 				?>
 		</article>
-	</main>
+		</section>
+<!-- 			<footer>
+				<center><div class="informations_footer">
+					<p>NODEX - 14, rue du Général de Gaule - Z.I. Le Pré vert - 39500 Moirans-en-Montagne<br>
+					Tél: 03.80.45.78.66<br>
+					Copyright © NODEX-Industry</p>
+				</div></center>
+			</footer> -->
 	</body>
 </html>
